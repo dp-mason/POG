9edf44c6854ab77b338bac2d51ffca48b8cb9b2e
David Mason <54563107+dp-mason@users.noreply.github.com>
2021-04-15 00:56:09 -0500

    Update parse.sh

--- back_end/rest-service/tmp/parse.sh
+++ back_end/rest-service/tmp/parse.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-echo "Parsing..."
-/usr/bin/python3 /home/davidpattenmason/papersOG/POG/back_end/rest-service/tmp/backend_scholar_scraper.py
+while true; do
+    inotifywait --monitor /home/david/Desktop/sda1/Senior/misc_pog_stuff/POG-main/POG-main/back_end/rest-service/tmp -e create -e moved_to |
+        python3 /home/david/Desktop/sda1/Senior/misc_pog_stuff/POG-main/POG-main/back_end/rest-service/tmp/backend_scholar_scraper.py
+done
