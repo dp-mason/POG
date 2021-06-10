997c337413f34e1724450073118e001740b580a6
dp-mason <davidpattenmason@gmail.com>
2021-06-10 22:59:59 +0000

    soon to be removed shell script fix :)

--- back_end/rest-service/tmp/parse.sh
+++ back_end/rest-service/tmp/parse.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while true; do
-    inotifywait --monitor /home/davidpattenmason/POG-DB/back_end/rest-service/tmp -e create -e moved_to |
-        python3 /home/davidpattenmason/POG-DB/back_end/rest-service/tmp/backend_scholar_scraper.py
+    inotifywait --monitor /home/davidpattenmason/POG-PostGrad/back_end/rest-service/tmp -e create -e moved_to |
+        python3 /home/davidpattenmason/POG-PostGrad/back_end/rest-service/tmp/backend_scholar_scraper.py
 done
