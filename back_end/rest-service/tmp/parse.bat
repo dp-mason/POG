aaccd6d837ec2262261aba6c7c73c73826c98ae2
David Mason <54563107+dp-mason@users.noreply.github.com>
2021-06-18 14:49:41 -0500

    sockets work, no need for tmp

--- back_end/rest-service/tmp/parse.bat
+++
@@ -1,1 +0,0 @@
-python C:\Users\IEUser\Desktop\POG\back_end\rest-service\tmp\backend_scholar_scraper.py
--- back_end/rest-service/tmp/parse.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while true; do
-    inotifywait --monitor /home/davidpattenmason/POG-PostGrad/back_end/rest-service/tmp -e create -e moved_to |
-        python3 /home/davidpattenmason/POG-PostGrad/back_end/rest-service/tmp/backend_scholar_scraper.py
-done
